341305468fc6101f33602da7313b447e208c87eb
Marc Abramowitz <marc@marc-abramowitz.com>
2012-04-24 10:25:14 -0700

    Lazy import biplist so it doesn't cause travis-ci to fail

--- anyserializer/serializers/biplistserializer.py
+++ anyserializer/serializers/biplistserializer.py
@@ -1,5 +1,3 @@
-import biplist
-
 from ..serializer import Serializer
 
 
@@ -7,6 +5,8 @@ class BiplistSerializer(Serializer):
 
     def __init__(self):
         super(BiplistSerializer, self).__init__()
+        import biplist
+        self.biplist = biplist
 
     def serialize(self, obj, *args, **kwargs):
-        return biplist.writePlistToString(obj, *args, **kwargs)
+        return self.biplist.writePlistToString(obj, *args, **kwargs)
